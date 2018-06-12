7aaae8df90974732c103cde96f261315686d579b
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-12 11:45:22 +0300

    t

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
   for (var i = 0; i < d.childNodes.length; i++) {
     if (d.childNodes[i].nodeType === 1 ) {
       pCounter++;
-//    }
+    }
 //    if (pCounter === 2) {
 //      d.childNodes[i].innerHTML = "All his men.";
 //      break;
