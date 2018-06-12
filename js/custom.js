932e37b371216c968520cb70509f0fe3dd95779a
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-12 11:43:29 +0300

    t

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
   var d = document.getElementById("humpty");
   var pCounter = 0;
-//  for (var i = 0; i < d.childNodes.length; i++) {
+  for (var i = 0; i < d.childNodes.length; i++) {
 //    if (d.childNodes[i].nodeType === 1 ) {
 //      pCounter++;
 //    }
