06df9f0fba823e9101a4759b08539e13d9bf1365
mohammed yusuf <mwaigalo5@gmail.com>
2018-09-14 11:41:45 +0300

    t

--- js/custom.js
+++ js/custom.js
@@ -9,30 +9,30 @@
 //      break;
 //   }
 // } 
-function dr(){
-    var number=[];
-    for(i=0;i<5;i++){
-        var num=number.push(prompt("Enter a number"));
-    }
-    var count=0;
-    for(x=0;x<number.length;x++){
-        if(number[x]==9){
-            count++;
-        }
-        
-    }
-    if(count>=1){
-        alert(count);
-    }
-    else{
-        alert("There is no number 9 presence");
-    }
-    if(number==9){
-        alert("There is number nine ");
-    }
-    else{
-        alert("Theres no such number");
-    }
-    
-}
-dr();
+//function dr(){
+//    var number=[];
+//    for(i=0;i<5;i++){
+//        var num=number.push(prompt("Enter a number"));
+//    }
+//    var count=0;
+//    for(x=0;x<number.length;x++){
+//        if(number[x]==9){
+//            count++;
+//        }
+//        
+//    }
+//    if(count>=1){
+//        alert(count);
+//    }
+//    else{
+//        alert("There is no number 9 presence");
+//    }
+//    if(number==9){
+//        alert("There is number nine ");
+//    }
+//    else{
+//        alert("Theres no such number");
+//    }
+//    
+//}
+//dr();
