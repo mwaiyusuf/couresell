03645e6d01b3870df1018a3101ac462506a2542e
mwaiyusuf <mwaigalo5@gmail.com>
2018-11-05 23:44:54 +0300

    T

--- js/node.js
+++ js/node.js
@@ -7,8 +7,8 @@
 //  res.end('Hello World\n')
 //})
 //server.listen(port, hostname, () => {
-//  console.log(`Server running at http://${hostname}:${port}/`)
-//})
+ console.log(`Server running at http://${hostname}:${port}/`)
+})
 const http = require('http')
 const hostname = '127.0.0.1'
 const port = 3000
